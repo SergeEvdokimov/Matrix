960bbb69dc142e364b5acf7fc4a08185abb64dc9
Sergey <SergeyEwdokimow@ya.ru>
2020-02-09 13:32:56 +0300

    summ and sub complite

---
+++ Матрица/Матрица/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Матрица
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Matrix matrix = new Matrix(0,10);
+            string str = matrix.get_array();
+            richTextBox1.Text = str;
+        }
+    }
+}
---
+++ Матрица/Матрица/Matrix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Матрица
+{
+    class Matrix
+    {
+        public int[,] arr;
+        public Matrix()
+        {
+            arr = new int[5, 5];
+        }
+        public Matrix(int min, int max)
+        {
+            arr = new int[5, 5];
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    arr[i, j] = random.Next(min, max + 1);
+                }
+            }
+        }
+        public Matrix(int[,] array)
+        {
+            this.arr = array;
+        }
+
+        public string get_array()
+        {
+            string result = "";
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    result += arr[i, j] + " ";
+                }
+                result += "\n";
+            }
+            return result;
+
+        }
+
+    }
+}
